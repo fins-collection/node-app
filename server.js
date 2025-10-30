e9f17b6ff7e03b72c0b7dc479c524ee6a418f40c
australianastronautteam-sys <australianastronautteam@gmail.com>
2025-10-30 12:26:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,6 @@ const collectDefaultMetrics = prometheus.collectDefaultMetrics;
 const Registry = prometheus.Registry;
 const register = new Registry();
 collectDefaultMetrics({ register });
-// It must be placed above all other 'require' statements
-var atatus = require("atatus-nodejs");
-atatus.start({
-    licenseKey: "lic_apm_1c54876b5704409aa3eb76b79a6cd5ab",
-    appName: "checkout-service",
-    notifyHost: '10.40.31.24',
-    notifyPort: '8091',
-    analytics: true,
-    logBody: 'all',
-    analyticsCaptureOutgoing: true,
-    useSSL: false,
-    // logLevel: 'debug',
-});
-
 
 const express = require('express');
 const bodyParser = require('body-parser');
